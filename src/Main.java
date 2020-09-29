6a066d311d01208e50b8c9383f21afcc24e3fcf5
user <user@EB12>
2020-09-29 09:42:58 +0800

    My HomeWork

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("I'm Kevin Chen");
+		System.out.println("My nid is D0843556");
+		System.out.println("This is lab 1 hw");
+	}
+} 
